7afa8aa52c04b947b52742cffec76d94e2bf63cb
konrathi <i.konrath@gmx.at>
2022-09-14 15:07:40 +0200

    L4-data:initial

---
+++ Labs/L4-Data/food-app-mylab/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { SidemenuComponent } from "./shared/sidemenu/sidemenu.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidemenuComponent,
+    HomeComponent,
+    AboutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
