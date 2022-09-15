9ace56dcc2ec7973b0c4ba02544a6b49cc02e946
konrathi <i.konrath@gmx.at>
2022-09-15 09:28:57 +0200

    L5-ReactiveForms: Update Reactive Forms

--- Labs/L5-ReactiveForms/food-app-mylab/src/app/food/food-edit/food-edit.component.ts
+++ Labs/L5-ReactiveForms/food-app-mylab/src/app/food/food-edit/food-edit.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { FormGroup, UntypedFormBuilder, Validators } from "@angular/forms";
-import { FoodItem } from "../food.model";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FoodItem } from '../food.model';
 
 @Component({
-  selector: "app-food-edit",
-  templateUrl: "./food-edit.component.html",
-  styleUrls: ["./food-edit.component.scss"],
+  selector: 'app-food-edit',
+  templateUrl: './food-edit.component.html',
+  styleUrls: ['./food-edit.component.scss'],
 })
-export class FoodEditComponent implements OnInit {
-  
-  foodItem: FoodItem = new FoodItem();
-  
-  foodForm = this.fb.group({
-    name: [this.foodItem.name, Validators.required],
-    price: [this.foodItem.price],
-    calories: [this.foodItem.calories],
-  });
-
-  constructor(private fb: UntypedFormBuilder) {}
-
+export class FoodEditComponent {
   @Input() food: FoodItem = new FoodItem();
-  @Output() saveFood: EventEmitter<FoodItem> = new EventEmitter<FoodItem>();
+  @Output() saveFood: EventEmitter<FoodItem> = new EventEmitter();
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      id: this.food.id,
+      name: [this.food.name, [Validators.required, Validators.minLength(3)]],
+      price: [this.food.price, [Validators.required, Validators.min(1)]],
+      calories: this.food.calories,
+    });
+  }
 
-  doSave() {
-    console.log("food to save", this.food);
-    this.saveFood.emit(this.food);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['food']) {
+      this.form.setValue(changes['food'].currentValue);
+    }
+  }
+
+  saveForm(form: any) {
+    console.log('food to save', form.value);
+    this.saveFood.emit(form.value);
   }
 }
